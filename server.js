84f2fa589ce2b61833a100006017ff70b5375065
ola466@gmail.com <ola466@gmail.com>
2025-11-02 01:02:12 -0600

    connect project to database

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongodb= require('./routes/data/database')
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(` Database is listening and node  Running on port ${port}`);
+        });}
+});
+
+
+
+
+
+
