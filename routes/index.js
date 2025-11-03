792655c16e6d551a57841ce281e6f9c9ad7601c1
ola466@gmail.com <ola466@gmail.com>
2025-11-03 00:11:57 -0600

    Added getAll and getSingle routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => { res.send('Api is running') });
+router.get('/', (req, res) => {
+    res.redirect('/contacts')
+}); // Redirect root to contacts            );
 
 router.use('/contacts', require('./contacts'));
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongodb= require('./data/database')
 const app = express();
 
 const port = process.env.PORT || 3000;
+app.use(express.json());
 
 app.use('/', require('./routes'));
 
